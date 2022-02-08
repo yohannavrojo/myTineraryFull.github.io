57972581fccbf1f033d96286c70422ac693676f1
Yohanna Valeska Rojo Noguera <yohavale7883@gmail.com>
2022-02-08 14:04:46 -0300

    cambio en database

--- mytineray-main/backend/config/database.js
+++ mytineray-main/backend/config/database.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose")
 
 mongoose.connect(process.env.MONGO_URI, {
     useUnifiedTopology: true,
-    useNewUrlParser: true,
+    useNewUrlParser: true
   })
 
   .then(() => console.log("Base de datos conectadas"))
-  .catch((error => console.error(error)));
+  .catch((error => console.error(error)))
