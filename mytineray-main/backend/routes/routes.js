edb6baef71e6299cb5535c8fbebfff91ab52a0a2
Yohanna Valeska Rojo Noguera <yohavale7883@gmail.com>
2022-02-09 15:47:04 -0300

    actualizado routers

--- mytineray-main/backend/routes/routes.js
+++ mytineray-main/backend/routes/routes.js
@@ -1,8 +1,11 @@
 const Router = require("express").Router();
 const citiesController = require("../controller/datosControllers")// revisar esta ruta 
-const {ObtenerTodosLosDatos} = citiesController // desestructuración del controlador de Ciudades
+const {ObtenerTodosLosDatos, ObtenerItynerarios} = citiesController // desestructuración del controlador de Ciudades
 
 Router.route("/datos") // "datos" parte de la url de la consulta
 .get(ObtenerTodosLosDatos)
+
+Router.route("/itinerary") // "datos" parte de la url de la consulta
+.get(ObtenerItynerarios)
 
 module.exports = Router
