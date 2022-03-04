b477ce917544995686481f22e53ea2a5dbbb5fd7
Yohanna Valeska Rojo Noguera <yohavale7883@gmail.com>
2022-03-04 18:23:22 -0300

    route accesoUsuario

--- mytineray-main/backend/routes/routes.js
+++ mytineray-main/backend/routes/routes.js
@@ -1,9 +1,11 @@
 const Router = require("express").Router();
-const datosController = require("../controller/datosControllers")// revisar esta ruta 
+const datosController = require("../controller/datosControllers");// revisar esta ruta 
+const { accesoUsuario } = require("../controller/usersControllers.js");
 const {ObtenerTodosLosDatos, ObtenerItynerarios} = datosController // desestructuración del controlador de Ciudades
 const usersController= require ("../controller/usersControllers.js")
-const {nuevoUsuario, verifyEmail} = usersController
+const {nuevoUsuario, verifyEmail,accesoUsuario} = usersController
 const validator= require("../controller/validator.js")
+
 
 Router.route("/datos") // "datos" parte de la url de la consulta
 .get(ObtenerTodosLosDatos)
@@ -16,4 +18,9 @@ Router.route("/signup")
 
 Router.route("/verify/:uniqueText") // "datos" parte de la url de la consulta 
 .get(verifyEmail)
+
+Router.route("/singin") // "datos" parte de la url de la consulta 
+.post(accesoUsuario)
+
+
 module.exports = Router
