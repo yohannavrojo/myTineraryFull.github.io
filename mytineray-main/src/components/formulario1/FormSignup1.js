91de07c5bc7533b0aedc106a48938a931ae0d728
Yohanna Valeska Rojo Noguera <yohavale7883@gmail.com>
2022-03-11 13:22:31 -0300

    arreglando signup

--- mytineray-main/src/components/formulario1/FormSignup1.js
+++ mytineray-main/src/components/formulario1/FormSignup1.js
@@ -1,10 +1,77 @@
 import React from 'react';
 import './Form1.css';
 import { Link } from "react-router-dom";
+// import { actionType } from "../../reducer";
+// import { useStateValue } from '../../StateProvider';
 import axios from 'axios';
-
+// import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from 'react-google-login';
 
 const FormSignup = () => {
+ /* SignUp con Google start */
+  
+ const responseGoogle = async (response) => {
+    console.log(response);
+    const googleUser = {
+      firstname: response.profileObj.givenName,
+      lastname: response.profileObj.familyName,
+      email: response.profileObj.email,
+      password:response.googleId ,
+      google:true,
+      from:"Google"
+    }
+    
+    await axios.post("http://localhost:4000/api/signup",{googleUser} )
+   .then(response=>    
+   displayMessages(response.data)
+ )
+ function displayMessages(data){
+  if(data.success==="falseVAL"){
+    console.log(data)
+   console.log(data.response.error.details)
+  alert(data.response.error.details.map(error=>error.message))
+  }else if(data.success===true){
+   
+    console.log(data)
+  }
+ }
+    
+  } 
+
+/* sign up de facebook */
+
+// const responseFacebook = async (response) => {
+//   console.log(response);
+
+//   const NuevoUsuario = {
+//                       email:response.email,
+//                       firstname:response.name,
+//                       lastname:"facebook",            
+//                       password:response.id + "aB", 
+//                       from:"Facebook"    
+//   }  
+
+//     await axios.post("http://localhost:4000/api/signup",{NuevoUsuario} )
+//     .then(response=> //alert(response.data.response)) 
+
+
+//     displayMessages(response.data)
+//     )
+
+    
+//     function displayMessages(data){
+//     if(data.success==="falseVAL"){
+//         console.log(data)
+//         console.log(data.response.error.details)
+//         alert(data.response.error.details.map(error=>error.message))
+//     }else if(data.success===true){
+        
+//     console.log(data)
+//     }
+//   }
+// }
+
+
 
 async function NewUser(event){
   console.log(event)
@@ -14,6 +81,7 @@ async function NewUser(event){
     lastname:event.target[1].value,
     email:event.target[2].value,
     password:event.target[3].value,
+    google:false
 }
   console.log(NuevoUsuario)
 
@@ -113,6 +181,20 @@ async function NewUser(event){
               </Link>
         </span>
       </form>
+
+      <GoogleLogin
+          clientId="971845975096-d96pfrveho1431brgjcu4m4a2leibuei.apps.googleusercontent.com"
+          buttonText="login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+  cookiePolicy={'single_host_origin'}/>,
+
+{/* <FacebookLogin
+          appId="1062880977629069"
+          autoLoad={true}
+          fields="name,email,picture"        
+          callback={responseFacebook} /> */}
+
     </div>
   );
 }
