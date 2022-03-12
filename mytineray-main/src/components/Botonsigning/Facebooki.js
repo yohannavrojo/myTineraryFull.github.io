598eb3dc756ec93dfce27d88bdf4a99532c5b57c
Yohanna Valeska Rojo Noguera <yohavale7883@gmail.com>
2022-03-12 19:40:02 -0300

    botones sign in

---
+++ mytineray-main/src/components/Botonsigning/Facebooki.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import FacebookLogin from "react-facebook-login";
+import axios from 'axios';
+
+
+function Facebooki() {
+
+
+const responseFacebook = async (response) => {
+    
+    // console.log(response);
+    const NuevoUsuario = {
+                        email:response.emai,           
+                        password:response.id + "Ab", 
+                        from:"Facebook"    
+    } ; 
+
+      await axios.post("http://localhost:4000/api/signup",{NuevoUsuario} )
+      .then(response=> //alert(response.data.response)) 
+
+
+      displayMessages(response.data)
+      
+      )
+console.log(response.data)
+      
+      function displayMessages(data){
+      if(data.success==="falseVAL"){
+        console.log(data);
+        // console.log(data.response.error.details);
+          alert(data.response.error.details.map(error=>error.message))
+      }else if(data.success===true){
+         
+      console.log(data)
+      }
+    }
+  }
+
+
+return(
+    
+    <div className='google mt-3'>
+    <FacebookLogin
+  appId="1569533133424777"
+  autoLoad={false}
+  fields="name,email,picture"
+  callback={responseFacebook} />
+
+
+</div>
+);
+
+}
+
+export default Facebooki;
+
+
---
+++ mytineray-main/src/components/Botonsigning/Googlei.js
@@ -0,0 +1,46 @@
+import React from "react";
+import GoogleLogin from 'react-google-login';
+import axios from 'axios';
+
+
+function Googlei(){ 
+  
+const responseGoogle = async (response) => {
+   
+    const NuevoUsuario = {
+      email: response.profileObj.email,
+      password:response.googleId+"Ep" ,
+      from:"google"
+      
+    }
+
+    await axios.post("http://localhost:4000/api/signup",{NuevoUsuario})
+   .then(response=>
+   displayMessages(response.data),
+  
+  )
+  function displayMessages(data){
+    if(data.success==="falseVAL"){
+      console.log(data)
+      console.log(data.response.error.details)
+    alert(data.response.error.details.map(error=>error.message))
+    
+  } else if(data.success==="trueUE"){
+     console.log(data)
+    }
+  }
+  
+  }
+
+  return(
+<GoogleLogin
+          clientId="971845975096-d96pfrveho1431brgjcu4m4a2leibuei.apps.googleusercontent.com"
+          buttonText="SignUp with Google Account"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+  cookiePolicy={'single_host_origin'}/>
+  )
+}
+  
+
+  export default Googlei;
