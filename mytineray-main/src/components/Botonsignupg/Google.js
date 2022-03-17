e5b02c34eab31bd5a853ec7cacf98e2e60106ec9
Yohanna Valeska Rojo Noguera <yohavale7883@gmail.com>
2022-03-17 15:08:16 -0300

    arreglado google signup

--- mytineray-main/src/components/Botonsignupg/Google.js
+++ mytineray-main/src/components/Botonsignupg/Google.js
@@ -5,54 +5,53 @@ import axios from 'axios';
 
 function Google(){ 
   
-const responseGoogle = async (response) => {
-   
-    const NuevoUsuario = {
-      firstname: response.profileObj.givenName,
-      lastname: response.profileObj.familyName,
-      email: response.profileObj.email,
-      password:response.googleId+"Ep" ,
-      from:"google"
-      
-    }
-    
+  const responseGoogle = async (response) => {
+      console.log(response);
 
-    await axios.post("http://localhost:4000/api/signup",{NuevoUsuario})
-   .then(response=>
-   displayMessages(response.data),
-  
-  )
-  // function displayMessages(data){
-  //   if(data.success==="falseVAL"){
-  //     console.log(data)
-  //     console.log(data.response.error.details)
-  //   alert(data.response.error.details.map(error=>error.message))
-    
-  // } else if(data.success==="trueUE"){
-  //    console.log(data)
-  //   }
-  // }
-  function displayMessages(data){
-    if(data.success==="falseVAL"){
-      console.log(data);
-      // console.log(data.response.error.details);
-        alert(data.response.error.details.map(error=>error.message))
-    }else if(data.success===true){
-       
-    console.log(data)
-    }
-  }
-  
+
+      const NuevoUsuario = {
+          firstname: response.profileObj.givenName,
+          lastname: response.profileObj.familyName,
+          email: response.profileObj.email,
+          password: response.googleId + "Ka",
+          from:"Google", //controladores
+      }
+      await axios.post("http://localhost:4000/api/signup",{NuevoUsuario })
+          .then(response => //alert(response.data.response))
+              //  if (response.success === "falseVAL"){
+              // console.log(response.data)
+              // }
+              displayMessages(response.data) //para validar los datos
+
+          )
+      function displayMessages(data) {
+          if (data.success === "falseVAL") {
+              //  console.log(data)
+              // console.log(data.response.error.details)
+              alert(data.response.error.details.map(error => error.message))
+          } else if (data.success === true) {
+              console.log(data)
+          }
+
+      }
   }
 
+ 
+  
   return(
-<GoogleLogin
-          clientId="971845975096-d96pfrveho1431brgjcu4m4a2leibuei.apps.googleusercontent.com"
-          buttonText="SignUp with Google Account"
-          onSuccess={responseGoogle}
-          onFailure={responseGoogle}
-  cookiePolicy={'single_host_origin'}/>
+
+     
+      <GoogleLogin
+      clientId="971845975096-d96pfrveho1431brgjcu4m4a2leibuei.apps.googleusercontent.com"
+      buttonText="singup WITH GOOGLE"
+      onSuccess={responseGoogle}
+      onFailure={responseGoogle}
+      cookiePolicy={'single_host_origin'}
+    />
+    
   )
+
+
 }
   
 
