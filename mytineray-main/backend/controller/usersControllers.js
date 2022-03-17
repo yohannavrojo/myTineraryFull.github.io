08849e87cc0a1f9ec505e4148fdce3f5a9e9dec4
Yohanna Valeska Rojo Noguera <yohavale7883@gmail.com>
2022-03-17 15:38:05 -0300

    cambios facebook

--- mytineray-main/backend/controller/usersControllers.js
+++ mytineray-main/backend/controller/usersControllers.js
@@ -179,6 +179,7 @@ const usersController = {
 
   },
 
+  
 cerrarsesion: async (req, res) => {
 
     const email = req.body.email
--- mytineray-main/src/components/Botonsignupg/Facebook.js
+++ mytineray-main/src/components/Botonsignupg/Facebook.js
@@ -9,28 +9,27 @@ const responseFacebook = async (response) => {
 
    const NuevoUsuario = {
        firstname: response.name,
-       lastname: response.lastname,
+       lastname:  "facebook",
        email: response.email,
        password: response.id + "Fa",
        from: "facebook"//si el usuario se carga a través de google no se pide la verificación
    }
 
    await axios.post("http://localhost:4000/api/signup",{NuevoUsuario })
-       .then(response =>
-          
+   .then(response => //alert(response.data.response))
+     
+       displayMessages(response.data) //para validar los datos
 
-          displayMessages(response.data) //para validar los datos
-
-       )
-   function displayMessages(data) {
-       if (data.success === "falseVAL") {
-           
-           alert(data.response.error.details.map(error => error.message))
-       } else if (data.success === true) {
-           console.log(data)
-       }
-
+   )
+function displayMessages(data) {
+   if (data.success === "falseVAL") {
+       
+       alert(data.response.error.details.map(error => error.message))
+   } else if (data.success === true) {
+       console.log(data)
    }
+
+}
 }
 
 return (
