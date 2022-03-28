72547282095b796d3f638013e61fe10330e941ed
Yohanna Valeska Rojo Noguera <yohavale7883@gmail.com>
2022-03-28 11:09:24 -0300

    passport

--- mytineray-main/backend/config/passport.js
+++ mytineray-main/backend/config/passport.js
@@ -9,11 +9,12 @@ module.exports=passport.use(new jwtStrategy({
     jwtFromRequest:extractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey:process.env.SECRETKEY 
 },(jwt_payload,done)=>{
-    // console.log(jwt_payload)
+ // console.log(jwt_payload)
      User.findOne({_id:jwt_payload.id})
      .then(user=>{
          console.log(user)
          if (user) {
+             
              return done(null,user)
          }else if(error){
              return done(error,false)
