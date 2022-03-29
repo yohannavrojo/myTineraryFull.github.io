35226c6114dfc25cf8464c0ae3b12888f6aa5586
Yohanna Valeska Rojo Noguera <yohavale7883@gmail.com>
2022-03-29 19:51:18 -0300

    carousel home

---
+++ mytineray-main/src/components/general/CarouselData2.js
@@ -0,0 +1,24 @@
+import City1 from "../imagenes/dinamarca1.jpg";
+import City2 from "../imagenes/suiza1.jpg";
+import City3 from "../imagenes/espana1.jpg";
+import City4 from "../imagenes/roma1.jpg";
+import City5 from "../imagenes/irlanda1.jpg";
+import City6 from "../imagenes/argelia2.jpg";
+import City7 from "../imagenes/arggentina.jpg";
+import City8 from "../imagenes/bangladesh.jpg";
+import City9 from "../imagenes/caboverde.jpg";
+import City10 from "../imagenes/tailandia.jpeg";
+
+
+export const imagenes2 = [
+  { titlee: "Argelia", subtitle: "Hidden treasure of Europe", img: City6 },
+  { titlee: "Argentina", subtitle: "Beat to your rhythm", img: City7 },
+  {titlee: "Bangladesh",subtitle: "Beautiful Bangladesh",img: City8, },
+  {titlee: "Cape Verde",subtitle: "Meeting Cape Verde",img: City9, },
+  {titlee: "Denmark",subtitle: "The happiest place on Earth!",img: City1, },
+  { titlee: "Spain", subtitle: "Spain in detail", img: City3 },
+  { titlee: "Ireland", subtitle: "	Go to Ireland", img: City5 },
+  { titlee: "SSwitzerland", subtitle: "Get Natural", img: City2 },
+  { titlee: "Thailand", subtitle: "Incredible Thailand begins with the people", img: City10 },
+  { titlee: "Rome", subtitle: "Made in Italy", img: City4 },
+];
