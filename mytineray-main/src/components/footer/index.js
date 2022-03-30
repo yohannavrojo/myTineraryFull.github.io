a59c99770d5965db18f432cdaf95262521829792
Yohanna Valeska Rojo Noguera <yohavale7883@gmail.com>
2022-03-30 15:41:47 -0300

    nada

--- mytineray-main/src/components/footer/index.js
+++ mytineray-main/src/components/footer/index.js
@@ -6,6 +6,7 @@ export default function Footer ({children, ...restProps})
 {
 return <Container {... restProps}>{children}</Container>    
 }
+
 Footer.Wrapper = function FooterWrapper({children, ...restProps}){
     return <Wrapper {...restProps}>{children}</Wrapper> 
 }
